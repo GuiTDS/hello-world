9e358e39d9bca35c9e6b33aaf5475b51566e4280
Guilherme Sanches <guitdsanches@gmail.com>
2023-04-12 08:46:32 -0300

    adding react-markdown

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,7 +1,8 @@
 import { useParams } from 'react-router-dom';
 import posts from 'json/posts.json';
 import PostModelo from 'componentes/PostModelo';
-
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import './Post.css';
 
 const Post = () => {
     const parametros = useParams();
@@ -13,7 +14,13 @@ const Post = () => {
 
     return ( 
         <PostModelo fotoCapa={`/assets/posts/${post.id}/capa.png`} titulo={post.titulo} >
-            text...
+            <div className='post-markdown-container'>
+                <ReactMarkdown>
+                    {post.texto}    
+                </ReactMarkdown>   
+            </div>
+            
+            
         </PostModelo>
      );
 }
